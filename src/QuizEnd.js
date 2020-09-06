a3dc2400a1bb555a90ed3823aafec5d1d8bf2678
thiago mendes <thimens@gmail.com>
2020-09-05 18:27:55 -0700

    added quizend

---
+++ src/QuizEnd.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Quiz from './Quiz';
+
+class QuizEnd extends Component
+{
+    render() {
+        return (
+            <div>
+                <p>Thanks for playing!</p>
+                <a href=''>Reset Quiz</a>
+            </div>
+        );
+    }
+}
+
+export default QuizEnd;
